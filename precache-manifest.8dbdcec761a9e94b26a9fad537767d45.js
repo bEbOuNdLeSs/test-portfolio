7c3678edd21cf0bb8216e3c3b3544a8e03443fab
bEbOuNdLeSs <beboundlessssss@gmail.com>
2022-10-03 21:42:55 -0700

    Updates

---
+++ precache-manifest.8dbdcec761a9e94b26a9fad537767d45.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d45edf5c7fd73d6e17d7a89bf545712",
+    "url": "./index.html"
+  },
+  {
+    "revision": "09090a120bf61b10699d",
+    "url": "./static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "c4a6627e751d4fffc432",
+    "url": "./static/css/main.db6f0745.chunk.css"
+  },
+  {
+    "revision": "09090a120bf61b10699d",
+    "url": "./static/js/2.7a79c467.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "./static/js/2.7a79c467.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4a6627e751d4fffc432",
+    "url": "./static/js/main.eced9f2a.chunk.js"
+  },
+  {
+    "revision": "1d6896b13d1316b5acb7",
+    "url": "./static/js/runtime-main.2b4c0fc3.js"
+  }
+]);
